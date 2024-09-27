f33cfc1679c7459fd536622dce1315e1a6b29ddd
Jhack Alberth Caceres Hilasaca <111236563+Jhack42@users.noreply.github.com>
2024-09-26 21:20:44 -0500

    Descripción de los cambios realizados

--- api_javascript/app.js
+++ api_javascript/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', routes);
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
