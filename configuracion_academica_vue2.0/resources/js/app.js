5c4a10dd43f71d42d794e0ec4c1c5145381624ae
Jhack Alberth Caceres Hilasaca <111236563+Jhack42@users.noreply.github.com>
2024-12-05 08:25:49 -0500

    Descripción de los cambios realizados

--- configuracion_academica_vue2.0/resources/js/app.js
+++
@@ -1,8 +0,0 @@
-// resources/js/app.js
-
-import './bootstrap';
-import { createApp } from 'vue'; // Vue 3
-import App from './components/App.vue';
-
-const app = createApp(App);
-app.mount('#app');
--- configuracion_academica_vue2.0/webpack.mix.js
+++
@@ -1,7 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-    .vue({ version: 3 })  // Asegúrate de que la versión de Vue sea 3
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ]);
